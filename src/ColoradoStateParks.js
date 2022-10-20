c11f6e470d079edc2f0be28ac278220a1d4e014c
Ty Blackwell <tyeverettblackwell@gmail.com>
2022-10-20 15:06:28 -0400

    commit tree 3f182d0d94373835cb37cb7e80f5f207a8eca12c

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,16 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
+
+export default ColoradoStateParks
--- src/parks/RockyMountain.js
+++ src/parks/RockyMountain.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const trees = "Aspen and Pine";
 
 function wildlife() {
